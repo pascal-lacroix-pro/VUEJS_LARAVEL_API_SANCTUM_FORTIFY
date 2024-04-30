7826ae6dd89430d9370bdd203e7c92bc8084259a
Pascal Lacroix <p.lacroix@promotion-sociale.be>
2024-04-30 15:38:54 +0200

    Configuration initiales
    BACKEND: sanctum, fortify, cors
    FRONTE?D: axios

---
+++ FRONTEND/src/axios.defaults.js
@@ -0,0 +1,13 @@
+axios.defaults.baseURL = 'http://localhost:8000/';
+
+// Définir des en-têtes par défaut pour toutes les requêtes
+axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+axios.defaults.headers.common['Content-Type'] = 'application/json';
+axios.defaults.headers.common['Accept'] = 'application/json';
+
+// Permettre l'envoi des cookies lors des requêtes cross-domain
+axios.defaults.withCredentials = true;
+axios.defaults.withXSRFToken = true;
+
+// Exporter axios pour l'utiliser dans vos composants
+export default axios;
--- FRONTEND/src/main.js
+++ FRONTEND/src/main.js
@@ -5,6 +5,7 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import './axios.defaults'
 
 const app = createApp(App)
 
