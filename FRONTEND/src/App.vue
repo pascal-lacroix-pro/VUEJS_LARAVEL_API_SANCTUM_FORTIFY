6ee3e78d2340cd743fbb0552894b907b60de5d5b
Pascal Lacroix <p.lacroix@promotion-sociale.be>
2024-04-30 14:57:51 +0200

    Installations initiales
    BACKEND:: Laravel 11, Sanctum, Fortify
    FRONTEND Vuejs, Vue Router, Pinia, Axios, Tailwind

---
+++ FRONTEND/src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <header>
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="@/assets/logo.svg"
+      width="125"
+      height="125"
+    />
+
+    <div class="wrapper">
+      <h1 class="text-red-500 text-3xl">Test</h1>
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
